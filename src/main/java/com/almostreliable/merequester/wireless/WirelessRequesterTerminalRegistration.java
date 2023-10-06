bbd619eee2bdffaf7f65195a8df1b71bda69ae1c
Mari023 <38946771+Mari023@users.noreply.github.com>
2023-10-06 13:13:42 +0200

    add hotkey when not using ae2wtlib add hotkey localization

--- src/main/java/com/almostreliable/merequester/wireless/WirelessRequesterTerminalRegistration.java
+++ src/main/java/com/almostreliable/merequester/wireless/WirelessRequesterTerminalRegistration.java
@@ -4,6 +4,8 @@ import appeng.api.features.GridLinkables;
 import appeng.api.upgrades.Upgrades;
 import appeng.core.definitions.AEItems;
 import appeng.core.localization.GuiText;
+import appeng.hotkeys.HotkeyActions;
+import appeng.hotkeys.InventoryHotkeyAction;
 import appeng.items.tools.powered.WirelessTerminalItem;
 import com.almostreliable.merequester.Utils;
 import net.minecraft.core.Registry;
@@ -18,5 +20,7 @@ public class WirelessRequesterTerminalRegistration {
         GridLinkables.register(WIRELESS_REQUESTER_TERMINAL, WirelessTerminalItem.LINKABLE_HANDLER);
         Registry.register(Registry.MENU, Utils.getRL(WirelessRequesterTerminalMenu.ID), WirelessRequesterTerminalMenu.TYPE);
         Upgrades.add(AEItems.ENERGY_CARD, WIRELESS_REQUESTER_TERMINAL, 2, GuiText.WirelessTerminals.getTranslationKey());
+        HotkeyActions.register(new InventoryHotkeyAction(WIRELESS_REQUESTER_TERMINAL,
+            (player, i) -> WIRELESS_REQUESTER_TERMINAL.openFromInventory(player, i)), "wireless_requester_terminal");
     }
 }
