a7a15d091875f698170a7bbc4882666fbf484e07
rlnt <16513358+rlnt@users.noreply.github.com>
2026-09-01 19:28:47 +0200

    mark slot field accessors as mutable

--- src/main/java/com/almostreliable/merequester/mixin/accessors/SlotMixin.java
+++ src/main/java/com/almostreliable/merequester/mixin/accessors/SlotMixin.java
@@ -2,15 +2,19 @@ package com.almostreliable.merequester.mixin.accessors;
 
 import net.minecraft.world.inventory.Slot;
 
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
 @Mixin(Slot.class)
 public interface SlotMixin {
 
     @Accessor("x")
+    @Mutable
     void merequester$setX(int x);
 
     @Accessor("y")
+    @Mutable
     void merequester$setY(int y);
 }
